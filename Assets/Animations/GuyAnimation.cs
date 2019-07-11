0ee7f1c9328594ef887fa0bd56b0fd2e52c6b0c3
Micah Burt <micahcburt@gmail.com>
2019-07-10 18:15:59 -0700

    First commit

---
+++ Assets/Animations/GuyAnimation.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuyAnimation : MonoBehaviour
+{
+    private Animator myAnimator;
+    private bool isShooting = false;
+    public float noButtonPress = 0.0f;
+    private float buttonReset = 0.0f;
+
+
+    private void Start()
+    {
+       myAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (isShooting == false)
+        {
+            if(noButtonPress >= 6)
+            {
+                noButtonPress = buttonReset;
+                myAnimator.SetFloat("noButtonPress", buttonReset);
+            }
+            else
+            {
+                noButtonPress = noButtonPress + 1 * Time.deltaTime;
+                myAnimator.SetFloat("noButtonPress", noButtonPress);
+            }
+           
+            
+        }
+        else if (isShooting == true)
+        {
+            noButtonPress = buttonReset;
+            myAnimator.SetFloat("noButtonPress", buttonReset);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isShooting)
+        {
+            myAnimator.SetBool("isShooting", true);
+            isShooting = true;
+        }
+
+        else if (Input.GetKeyUp(KeyCode.Space) && isShooting)
+        {
+            myAnimator.SetBool("isShooting", false);
+            isShooting = false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This script moves the character controller forward
+// and sideways based on the arrow keys.
+// It also jumps when pressing space.
+// Make sure to attach a character controller to the same game object.
+// It is recommended that you make only one call to Move or SimpleMove per frame.
+
+public class Movement : MonoBehaviour
+{
+
+    Vector3 movement;
+    public float speed = 20f;
+    int floorMask;
+    float camRayLength = 1000f;
+    Rigidbody playerRigidbody;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw ("Horizontal");
+        float v = Input.GetAxisRaw ("Vertical");
+
+        Move(h,v);
+        Turning();
+    }
+
+    void Move (float h, float v)
+    {
+        movement.Set (h, 0f, v);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition (transform.position + movement);
+
+    }
+
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorhit;
+
+        if (Physics.Raycast (camRay, out floorhit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorhit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+}
